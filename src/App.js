f0458e297d72421c21f4362d0f8246aa0a74f3c9
Anne-Marie Dufour <hello@annemariedufour.com>
2022-03-28 17:56:55 +0200

    Add markup from example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import './App.scss';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef } from "react";
+import './App.scss';
+import Quotes from './components/Quotes/Quotes';
+import Timeline from './components/Timeline/Timeline';
+import Conclusion from './components/Conclusion/Conclusion';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,52 +36,16 @@ function App() {
   return (
     <>
       <div className="container" ref={panelsContainer}>
-        <div
-          className="description panel blue"
-          ref={(e) => createPanelsRefs(e, 0)}
-        >
-          <div>
-            <h1>Horizontal snapping sections (simple)</h1>
-            <p>
-              Scroll vertically to scrub the horizontal animation. It also
-              dynamically snaps to the sections in an organic way based on the
-              velocity. The snapping occurs based on the natural ending position
-              after momentum is applied, not a simplistic "wherever it is when
-              the user stops".
-            </p>
-            <div className="scroll-down">
-              Scroll down<div className="arrow"></div>
-            </div>
-          </div>
-        </div>
-        <section className="panel red" ref={(e) => createPanelsRefs(e, 1)}>
-          ONE
+        <section className="panel panel-quotes" ref={(e) => createPanelsRefs(e, 0)}>
+          <Quotes />
         </section>
-        <section className="panel orange" ref={(e) => createPanelsRefs(e, 2)}>
-          TWO
+        <section className="panel panel-timeline" ref={(e) => createPanelsRefs(e, 1)}>
+          <Timeline />
         </section>
-        <section className="panel purple" ref={(e) => createPanelsRefs(e, 3)}>
-          THREE
-        </section>
-        <section className="panel green" ref={(e) => createPanelsRefs(e, 4)}>
-          FOUR
-        </section>
-        <section className="panel gray" ref={(e) => createPanelsRefs(e, 5)}>
-          FIVE
+        <section className="panel panel-conclusion" ref={(e) => createPanelsRefs(e, 2)}>
+          <Conclusion />
         </section>
       </div>
-
-      <header>
-        <a href="https://greensock.com/scrolltrigger">
-          <img
-            className="greensock-icon"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/scroll-trigger-logo-light.svg"
-            width="200"
-            height="64"
-            alt="GSAP"
-          />
-        </a>
-      </header>
     </>
   );
 }
